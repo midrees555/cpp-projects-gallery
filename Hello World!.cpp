8e859723782c55711ef1a8e143ecd0cabfce2f2d
midrees555 <115445554+midrees555@users.noreply.github.com>
2023-05-25 21:43:44 +0500

    Create Hello World!.cpp
    Simple C-plus-plus program to print Hello World using OOP (object oriented Programming) using c++.

---
+++ Hello World!.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+class HelloWorld
+{
+    public:
+    HelloWorld()
+    {
+        cout << "Hello World!" << endl;
+    }
+};
+
+int main()
+{
+    HelloWorld h1, h2, h3;
+    
+    return 0;
+}
